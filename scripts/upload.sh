ddfc60591746a64582ec920b9df72b4ee2ed9f78
Keita Kobayashi <keita@kbys.me>
2025-05-25 14:23:32 +0900

    Keep script, style uses this

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-rclone copyto ./osm-japan.pmtiles km-tileserver:km-tileserver/osm-japan.pmtiles --progress --s3-no-check-bucket --s3-chunk-size=256M
+rclone copyto ./osm-japan-filtered.pmtiles km-tileserver:km-tileserver/osm-japan.pmtiles --progress --s3-no-check-bucket --s3-chunk-size=256M
 echo "Uploaded latest basetiles to https://tiles.kmproj.com/osm-japan.json"
